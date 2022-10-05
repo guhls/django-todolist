b3a9a3f99c38743bb270963b9f242ce6c3a36692
Gustavo Silva <guhenriquels@gmail.com>
2022-10-04 21:45:49 -0300

    Created .env and added django secret key

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--q&kycvp3nmi19mzk)7+l6jm+1)v+s4bqr%j&9!t3kjeshu-84'  # noqa: E501
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'INSECURE')  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
