08c539f3e1baa1ceb29d39fe5a354a0bc8c9b974
Gustavo Silva <guhenriquels@gmail.com>
2022-10-06 12:36:01 -0300

    Created task Model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=60)
+    is_completed = models.BooleanField(default=False)
+    create_at = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=False, null=False)
