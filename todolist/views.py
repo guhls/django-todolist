487dc727d910a4718a4d71f312b767da5acb15b3
Gustavo Silva <guhenriquels@gmail.com>
2022-10-06 13:08:57 -0300

    Added tasks in context

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import Task
+
 
 def painel(request):
-    return render(request, 'todolist/pages/painel.html')
+    completed_tasks = Task.objects.filter(
+        author__username=request.user.username, is_completed=True)
+    uncompleted_tasks = Task.objects.filter(
+        author__username=request.user.username, is_completed=False)
+
+    return render(request, 'todolist/pages/painel.html', context={
+        'completed_tasks': completed_tasks,
+        'uncompleted_tasks': uncompleted_tasks,
+    })
