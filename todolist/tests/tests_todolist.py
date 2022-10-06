299776757fdb52b75ae15a3b5974725dee40b6c1
Gustavo Silva <guhenriquels@gmail.com>
2022-10-06 16:29:36 -0300

    Created testshow tasks in painel

--- todolist/tests/tests_todolist.py
+++ todolist/tests/tests_todolist.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from django.test import TestCase as DjangoTestCase
 from django.urls import resolve, reverse
 from todolist import views
+from todolist.models import Task, User
 
 
 class TestsTodoListUnittest(TestCase):
@@ -17,7 +18,33 @@ class TestsTodoListUnittest(TestCase):
 
 
 class TestTodoListDjango(DjangoTestCase):
+    def make_user(self, author):
+        user = User.objects.create(username=author, password='pass')
+        return user
+
+    def make_task(
+        self,
+        title='Title task',
+        author='user'
+    ):
+        task = Task.objects.create(
+            title=title,
+            author=self.make_user(author),
+        )
+        return task
+
     def test_painel_view_loads_template_is_correct(self):
         url = reverse('todolist:painel')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'todolist/pages/painel.html')
+
+    def test_task_fild_is_completed_is_false_by_default(self):
+        task = self.make_task(title='Title task', author='user')
+        self.assertFalse(task.is_completed)
+
+    def test_show_tasks_in_painel(self):
+        task = self.make_task()
+        url = reverse('todolist:painel')
+
+        response = self.client.get(url)
+        self.assertIn(task, response.context['uncompleted_tasks'])
