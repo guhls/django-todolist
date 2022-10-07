bb1b412bc5755d6fdec891078a86e94e641f1ef8
Gustavo Silva <guhenriquels@gmail.com>
2022-10-07 11:14:25 -0300

    Added edit view

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,7 +6,10 @@ app_name = 'todolist'
 
 urlpatterns = [
     path('painel/', views.painel, name='painel'),
+    path('task/<int:id>/', views.task_view, name='task-view'),
+
     path('create/', views.create_view, name='create'),
+    path('edit/', views.edit, name='edit'),
     path('done/', views.done, name='done'),
     path('undone/', views.undone, name='undone'),
     path('delete/', views.delete, name='delete'),
--- todolist/views.py
+++ todolist/views.py
@@ -15,6 +15,25 @@ def painel(request):
     })
 
 
+def task_view(request, id):
+    task = Task.objects.get(pk=id)
+
+    return render(request, 'todolist/pages/task-view.html', context={
+        'task': task,
+    })
+
+
+def edit(request):
+    task_id = request.GET.get('task-id')
+    task_title = request.GET.get('task-title')
+
+    task = Task.objects.get(pk=task_id)
+    task.title = task_title
+    task.save()
+
+    return redirect('todolist:painel')
+
+
 def create_view(request):
     title = request.GET.get('new-card-title')
     user = User.objects.get(pk=request.user.pk)
